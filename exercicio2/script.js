6272b06874a9c08ec45c1f50e6c678d6438bba12
vitorramos <ramos6382@gmail.com>
2022-08-29 14:56:18 -0300

    Exercicio 1 e 2

---
+++ exercicio2/script.js
@@ -0,0 +1,9 @@
+console.log ("Olá mundo!")
+
+let oculos = prompt("Vovê usa oculos?")
+let meias = prompt("Você esta de meias?")
+let deuMortal = prompt("Você ja deu um mortal")
+
+console.log (oculos)
+console.log (meias)
+console.log (deuMortal)
